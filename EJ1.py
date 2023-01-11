71fb15630844cd5f5ab6e8e1d9b528ba557686a0
UP210342 <up210342@alumnos.upa.edu.mx>
2023-01-10 22:58:48 -0600

    UP210342

---
+++ EJ1.py
@@ -0,0 +1,5 @@
+from fractions import Fraction
+
+x = 1
+y = Fraction(1, x + Fraction(1, x + Fraction( 1, x + Fraction(1, x))))
+print('y: ', y)
--- hip.py
+++ hip.py
@@ -1,10 +1,8 @@
 a = 3
 b = 4
-c = 0
 
 def raiz(x):
     y = x ** .5
     return y
 
-c = raiz(a**2 + b**2)
-print(c)
+print(raiz(a**2 + b**2))
